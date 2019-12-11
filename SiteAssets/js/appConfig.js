c5ebfd114a482f28133dbe9ba09b1d403b4b8999
Sergio F. Rodriguez <serrod@gmail.com>
2019-12-11 10:24:29 +0100

    changed KM to J69 office - error message

--- SiteAssets/js/appConfig.js
+++ SiteAssets/js/appConfig.js
@@ -62,7 +62,7 @@ var stepStatus = [
 	{caseStep: 'SUBMITTED', 					fwd: "DIR APPROVAL", 			fwdj6:"DIR APPROVAL", numerStep:2},
 	{caseStep: 'DIRECTORATE_APPROVAL', 			fwd: "BO APPROVAL", 			fwdj6:"BO APPROVAL", numerStep:3},
 	{caseStep: 'BILLING_OFFICIAL_APPROVAL', 	fwd: "PBO APPROVAL", 			fwdj6:"J6 APPROVAL", numerStep:4},
-	{caseStep: 'J6_APPROVAL', 					fwd: "ERROR: SEE KM", 			fwdj6:"PBO APPROVAL", numerStep:5},
+	{caseStep: 'J6_APPROVAL', 					fwd: "ERROR: SEE J69", 			fwdj6:"PBO APPROVAL", numerStep:5},
 	{caseStep: 'PBO_APPROVAL', 					fwd: "J8 APPROVAL", 			fwdj6:"J8 APPROVAL", numerStep:6},	
 	{caseStep: 'J8_APPROVAL', 					fwd: "CARD HOLDER VALIDATION",	fwdj6:"CARD HOLDER VALIDATION", numerStep:7},
 	{caseStep: 'CARD_HOLDER_VALIDATION', 		fwd: "REQUESTOR VALIDATION",	fwdj6:"REQUESTOR VALIDATION", numerStep:8},
