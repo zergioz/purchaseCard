53506e515b8cf3db3a5709eadbe5f449a521f35b
Ryan McLean <mcleanra@gmail.com>
2019-12-30 13:40:43 +0100

    added polyfills

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+//IE 11 polyfills
+import "babel-polyfill";
+import "whatwg-fetch";
+import "es6-promise/auto";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
