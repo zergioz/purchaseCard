d124a6de2e7f68ffe48dfd1ca590156fb9e20e0e
Ryan McLean <mcleanra@gmail.com>
2019-12-30 13:39:05 +0100

    generated interfaces for sp data

---
+++ src/services/models/interfaces/IPersonAttributes.ts
@@ -0,0 +1,14 @@
+export interface IPersonAttributes {
+  cardType: string;
+  cardId: string;
+  cardLimit: number;
+  currentExchange: number;
+  openDate: string;
+  closeDate: string;
+  cycleLimit: number;
+  spendingLimit: number;
+  personAgent: string;
+  levelFour: string;
+  levelFive: string;
+  billingOfficial: Array<string>;
+}
