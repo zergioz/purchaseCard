11fc2cb96c2429a052f527c4a69553465b1f7af2
Ryan McLean <mcleanra@gmail.com>
2019-12-30 13:41:33 +0100

    switch to ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -21,6 +19,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
 export default App;
