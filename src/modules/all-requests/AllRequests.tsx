65cb27cc110c6bdd0b58b4049db9035ed606ea32
Ryan McLean <mcleanra@gmail.com>
2019-12-31 14:30:54 +0100

    refactor

--- src/modules/all-requests/AllRequests.tsx
+++ src/modules/all-requests/AllRequests.tsx
@@ -1,55 +1,13 @@
 import React from "react";
-import { RequestTable } from "../../components/request-table/RequestTable";
-import { Request } from "../../services/models/Request";
-import { RequestService } from "../../services/RequestService";
-import { StepStatus } from "../../constants/StepStatus";
-import { SelectorPills } from "../../components/selector-pills/SelectorPills";
+import { RequestTableFiltered } from "../../components/request-table-filtered/RequestTableFiltered";
+import { Filters } from "../../components/requests-filter/Filters";
 
-const statuses: string[] = StepStatus.map(status => status.friendlyName);
-
-interface IProps {}
-
-interface IState {
-  requests?: Request[];
-  loading?: boolean;
-  directorate?: string | null;
-  status?: string | null;
-}
-
-export class AllRequests extends React.Component<IProps, IState> {
-  private svc: RequestService;
-  constructor(props: any) {
-    super(props);
-    this.state = { requests: [], status: null, loading: false };
-    this.svc = new RequestService();
-  }
-  componentWillMount() {
-    this.getData();
-  }
-
-  statusChanged(status: string) {
-    this.setState({ ...this.state, status: status });
-    this.getData();
-  }
-
-  getData() {
-    this.setState({ ...this.state, loading: true });
-    this.svc.read().subscribe((items: Request[]) => {
-      this.setState({ ...this.state, requests: items, loading: false });
-    });
-  }
-
-  render() {
-    return (
-      <React.Fragment>
-        <h1>All Requests</h1>
-        <hr />
-        <SelectorPills
-          values={statuses}
-          changeHandler={(status: string) => this.statusChanged(status)}
-        ></SelectorPills>
-        <RequestTable items={this.state.requests}></RequestTable>
-      </React.Fragment>
-    );
-  }
-}
+export const AllRequests: React.FC = () => {
+  return (
+    <React.Fragment>
+      <h1>All Requests</h1>
+      <hr />
+      <RequestTableFiltered filters={new Filters()} />
+    </React.Fragment>
+  );
+};
