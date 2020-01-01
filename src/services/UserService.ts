13b2314760b633433ee3149f02ff1b7d778eb200
Ryan McLean <mcleanra@gmail.com>
2020-01-01 18:02:37 +0100

    cleanup

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,16 +1,12 @@
 import dal from "./dal";
-import { ISerializer } from "./ISerializer";
 import { JsonStringSerializer } from "./JsonStringSerializer";
 import { Observable } from "rxjs";
-import { map, tap } from "rxjs/operators";
 
 export class UserService {
   private dal: dal;
-  private serializer: ISerializer;
 
   constructor() {
     this.dal = new dal();
-    this.serializer = new JsonStringSerializer();
     this.dal.getCurrentUser().subscribe();
   }
 
