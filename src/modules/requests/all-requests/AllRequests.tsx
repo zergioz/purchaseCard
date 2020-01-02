7c5758fdcf8f1046928d105545f3611d74b38eb0
Ryan McLean <mcleanra@gmail.com>
2020-01-02 15:38:22 +0100

    show item counts for each status

--- src/modules/requests/all-requests/AllRequests.tsx
+++ src/modules/requests/all-requests/AllRequests.tsx
@@ -1,16 +1,19 @@
-import React, { useEffect, useContext } from "react";
+import React, { useEffect, useContext, useState } from "react";
 import { Filters } from "../../../components/filters/Filters";
 import { RequestTable } from "../../../components/request-table/RequestTable";
 import { StatusFilter } from "../../../components/filters/StatusFilter";
 import { DirectorateFilter } from "../../../components/filters/DirectorateFilter";
 import RequestContext from "../../../contexts/RequestContext";
+import { Request } from "../../../services/models/Request";
 
 export const AllRequests: React.FC = () => {
   const context = useContext(RequestContext);
   const defaultFilters = new Filters();
 
+  const [filtered, setFiltered] = useState<Request[]>([]);
+
   useEffect(() => {
-    context.applyFilters(defaultFilters);
+    setFiltered(context.applyFilters(defaultFilters));
   }, []);
 
   return (
@@ -18,8 +21,8 @@ export const AllRequests: React.FC = () => {
       <h1>All Requests</h1>
       <hr />
       <DirectorateFilter />
-      <StatusFilter />
       <br />
+      <StatusFilter requestsToCount={filtered} />
       <RequestTable />
     </React.Fragment>
   );
--- src/modules/requests/requests-by-status/RequestsByStatus.tsx
+++ src/modules/requests/requests-by-status/RequestsByStatus.tsx
@@ -1,8 +1,9 @@
-import React, { useEffect, useContext } from "react";
+import React, { useEffect, useContext, useState } from "react";
 import { Filters } from "../../../components/filters/Filters";
 import { RequestTable } from "../../../components/request-table/RequestTable";
 import { StatusFilter } from "../../../components/filters/StatusFilter";
 import RequestContext from "../../../contexts/RequestContext";
+import { Request } from "../../../services/models/Request";
 
 interface IProps {
   status: string;
@@ -12,16 +13,19 @@ export const RequestsByStatus: React.FC<IProps> = props => {
   const context = useContext(RequestContext);
   const defaultFilters = new Filters();
 
+  const [filtered, setFiltered] = useState<Request[]>([]);
+
   useEffect(() => {
     defaultFilters.status = props.status;
-    context.applyFilters(defaultFilters);
+    let counts = context.applyFilters(defaultFilters);
+    setFiltered(counts);
   }, [props, context.requests]);
 
   return (
     <React.Fragment>
       <h1>Requests by Status</h1>
       <hr />
-      <StatusFilter />
+      <StatusFilter requestsToCount={filtered} />
       <br />
       <RequestTable />
     </React.Fragment>
