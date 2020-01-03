5daebbcc6cc41fcfa2bb6cf07adc11f2810e0c5c
Ryan McLean <mcleanra@gmail.com>
2020-01-03 12:21:10 +0100

    status filter updates on route change

--- src/components/filters/StatusFilter.tsx
+++ src/components/filters/StatusFilter.tsx
@@ -11,24 +11,34 @@ import { groupBy } from "../../helpers/GroupBy";
 const statuses: string[] = Object.keys(getStatusesByFriendlyName());
 
 interface IProps {
+  selected?: string;
   showBadgesFor?: Request[];
 }
 export const StatusFilter: React.FC<IProps> = props => {
   const context = useContext(RequestContext);
   const [badges, setBadges] = useState<number[]>([]);
-  const [selected, setSelected] = useState<string>("Submitted");
+  const [selected, setSelected] = useState<string>(
+    props.selected || "Submitted"
+  );
 
-  //recreate badges on props update
+  //recreate the request count badges if the props update
   useEffect(() => {
     const counts = countStatusGroups();
     setBadges(counts);
-    handleClick(selected);
   }, [props.showBadgesFor]);
 
-  const handleClick = (value: string) => {
-    context.applyFilters({ ...context.filters, status: value });
-    setSelected(value);
-  };
+  //if the status filter changes, we also want the tabs to change
+  useEffect(() => {
+    if (props.selected) {
+      const status = props.selected;
+      setSelected(status);
+    }
+  }, [props.selected]);
+
+  //if the state of this component changes, then apply the filters
+  useEffect(() => {
+    context.applyFilters({ ...context.filters, status: selected });
+  }, [selected]);
 
   //groups requests by status and counts them to make the badges
   const countStatusGroups = (): number[] => {
@@ -52,11 +62,16 @@ export const StatusFilter: React.FC<IProps> = props => {
   };
 
   const badgeStyle = "danger";
+
   return (
     <Nav fill variant="tabs" defaultActiveKey={selected}>
       {statuses.map((value: string, index: number) => (
         <Nav.Item key={`selector-${value}-${index}`}>
-          <Nav.Link onClick={() => handleClick(value)} eventKey={value}>
+          <Nav.Link
+            onClick={() => setSelected(value)}
+            eventKey={value}
+            active={value == selected}
+          >
             {value}{" "}
             <Badge variant={!!badges[index] ? badgeStyle : "light"}>
               {badges[index]}
