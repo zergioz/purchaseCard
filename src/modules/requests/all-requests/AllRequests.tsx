f7b6617e2290ee0ba739c501753732ec0b88d8e4
Ryan McLean <mcleanra@gmail.com>
2020-01-03 16:38:39 +0100

    all requests screen should clear the filters

--- src/modules/requests/all-requests/AllRequests.tsx
+++ src/modules/requests/all-requests/AllRequests.tsx
@@ -4,14 +4,21 @@ import { StatusFilter } from "../../../components/filters/StatusFilter";
 import { DirectorateFilter } from "../../../components/filters/DirectorateFilter";
 import RequestContext from "../../../contexts/RequestContext";
 import { RequestService } from "../../../services";
+import { Filters } from "../../../components/filters/Filters";
 
 export const AllRequests: React.FC = () => {
   const context = useContext(RequestContext);
+  const defaultFilters = new Filters();
 
   useEffect(() => {
     const svc = new RequestService();
     context.subscribeTo(svc.read());
   }, []);
+
+  useEffect(() => {
+    defaultFilters.status = "Submitted";
+    context.applyFilters(defaultFilters, true);
+  }, [context.requests]);
 
   return (
     <React.Fragment>
