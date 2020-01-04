358b6bb57fdcb86fecc920e3ca0b9e087bbe9777
Ryan McLean <mcleanra@gmail.com>
2020-01-04 10:55:59 +0100

    use custom breadcrumb to show active route

--- src/components/breadcrumbs/Breadcrumbs.tsx
+++ src/components/breadcrumbs/Breadcrumbs.tsx
@@ -1,53 +1,47 @@
-import React from "react";
+import React, { ReactElement } from "react";
 import modules from "../../modules";
 import { Breadcrumb } from "react-bootstrap";
 import { useLocation } from "react-router-dom";
+import { ModuleBreadcrumb } from "./ModuleBreadcrumb";
 
 export const Breadcrumbs = () => {
   const location = useLocation();
 
-  const makeBreadcrumbs = () => {
-    let crumbs: any = [];
+  const getBreadcrumbForModule = (module: any, key: any) => {
+    return <ModuleBreadcrumb module={module} key={`breadcrumb-${key}`} />;
+  };
+
+  const makeModuleBreadcrumbs = () => {
+    let crumbs: ReactElement<any>[] = [];
 
     modules.map((module: any, moduleIndex: number) => {
       if (location.pathname.indexOf(module.routeProps.path) != -1) {
-        crumbs.push(
-          <Breadcrumb.Item
-            href={`/#${module.routeProps.path}`}
-            key={`breadcrumb-module-${moduleIndex}`}
-          >
-            {module.name}
-          </Breadcrumb.Item>
-        );
+        const moduleCrumb = getBreadcrumbForModule(module, moduleIndex);
+        crumbs.push(moduleCrumb);
       }
 
       module.modules.map((submodule: any, submoduleIndex: number) => {
         if (location.pathname.indexOf(submodule.routeProps.path) != -1) {
-          crumbs.push(
-            <Breadcrumb.Item
-              href={`/#${submodule.routeProps.path}`}
-              key={`breadcrumb-submodule-${moduleIndex}-${submoduleIndex}`}
-            >
-              {submodule.name}
-            </Breadcrumb.Item>
+          const submoduleCrumb = getBreadcrumbForModule(
+            submodule,
+            moduleIndex * 100 + submoduleIndex * 10
           );
+          crumbs.push(submoduleCrumb);
         }
 
         submodule.links &&
           submodule.links.map((link: any, linkIndex: number) => {
             if (location.pathname.indexOf(link.routeProps.path) != -1) {
-              crumbs.push(
-                <Breadcrumb.Item
-                  href={`/#${link.routeProps.path}`}
-                  key={`breadcrumb-link-${moduleIndex}-${submoduleIndex}-${linkIndex}`}
-                >
-                  {link.name}
-                </Breadcrumb.Item>
+              const linkCrumb = getBreadcrumbForModule(
+                link,
+                moduleIndex * 100 + submoduleIndex * 10 + linkIndex
               );
+              crumbs.push(linkCrumb);
             }
           });
       });
     });
+
     return crumbs;
   };
 
@@ -55,7 +49,7 @@ export const Breadcrumbs = () => {
     <Breadcrumb>
       <Breadcrumb.Item href="/">Portal Home</Breadcrumb.Item>
       <Breadcrumb.Item href="#/">GPC Request System</Breadcrumb.Item>
-      {makeBreadcrumbs()}
+      {makeModuleBreadcrumbs()}
     </Breadcrumb>
   );
 };
