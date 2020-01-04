75d73397093f024df2171973df51f9e5f7c7d557
Ryan McLean <mcleanra@gmail.com>
2020-01-04 17:28:56 +0100

    status filter responsive-ish

--- src/components/filters/FilterControls.tsx
+++ src/components/filters/FilterControls.tsx
@@ -6,44 +6,43 @@ import { ClearFiltersButton } from "./ClearFiltersButton";
 import { StatusFilterProgressBar } from "./StatusFilterProgressBar";
 import { StatusFilterTabs } from "./StatusFilterTabs";
 import Media from "react-media";
+import { StatusFilter } from "./StatusFilter";
 
 export const FilterControls = () => {
   return (
     <>
-      <div className="container-fluid grey">
-        <div className="row">
-          <div className="col-md-12">
-            <small className="text-secondary">Filters</small>
-            <Card>
-              <Card.Body className="btn-group p-1">
-                <DirectorateFilter />
-                <FiscalYearFilter />
-                <ClearFiltersButton />
-              </Card.Body>
-            </Card>
+      <Media
+        queries={{
+          small: "(max-width: 599px)",
+          medium: "(min-width: 600px) and (max-width: 1199px)",
+          large: "(min-width: 1200px)"
+        }}
+      >
+        {(matches: any) => (
+          <div className="container-fluid grey">
+            <div className="row">
+              <div className="col-md-12">
+                <small className="text-secondary">Filters</small>
+                <Card>
+                  <Card.Body className="btn-group p-1">
+                    <DirectorateFilter />
+                    <FiscalYearFilter />
+                    {matches.small && <StatusFilter />}
+                    <ClearFiltersButton />
+                  </Card.Body>
+                </Card>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-lg-12">
+                <br />
+                {matches.medium && <StatusFilterTabs />}
+                {matches.large && <StatusFilterProgressBar />}
+              </div>
+            </div>
           </div>
-        </div>
-        <div className="row">
-          <div className="col-lg-12">
-            <br />
-            <Media
-              queries={{
-                small: "(max-width: 599px)",
-                medium: "(min-width: 600px) and (max-width: 1199px)",
-                large: "(min-width: 1200px)"
-              }}
-            >
-              {(matches: any) => (
-                <>
-                  {matches.small && <StatusFilterTabs />}
-                  {matches.medium && <StatusFilterTabs />}
-                  {matches.large && <StatusFilterProgressBar />}
-                </>
-              )}
-            </Media>
-          </div>
-        </div>
-      </div>
+        )}
+      </Media>
     </>
   );
 };
