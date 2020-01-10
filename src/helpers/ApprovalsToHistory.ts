70738dfd43d095c0248a4ab6cb7e6b0bd82d5022
Ryan McLean <mcleanra@gmail.com>
2020-01-10 12:28:14 +0100

    can finally parse legacy approvals

--- src/helpers/ApprovalsToHistory.ts
+++ src/helpers/ApprovalsToHistory.ts
@@ -11,18 +11,34 @@ import { ApprovalActions } from "../constants/ApprovalActions";
 import { IRequestApprovals } from "../services/models/Request";
 const statuses: StatusesByFriendlyName = getStatusesByFriendlyName();
 
+//gets the friendly status string for a legacy approval key based on the
+//information in the status constants
+export const getStatusForApprovalName = (approvalName: string): string => {
+  let foundKey = "";
+  Object.entries(statuses).find(([key, value]) => {
+    foundKey = key;
+    return value.approvalName == approvalName;
+  });
+  return foundKey;
+};
+
+//takes finds the legacy approval object that was created for a specific status
 export const getApprovalForStatus = (
   status: string,
   approvals: IRequestApprovals
-) => {
+): any => {
   const statusObj: IStatus = statuses[status];
   const approvalKey = statusObj.approvalName;
   const approval: any = approvals ? approvals[approvalKey] : undefined;
   return approval;
 };
 
+//yeah.
 export const parseApproval = (approval: any): ApprovalAction => {
-  let action: IApprovalAction = new ApprovalAction();
+  let type: string, formInputs: any, date: Date;
+  type = "noop";
+  formInputs = {};
+  date = new Date();
 
   if (approval) {
     if (
@@ -31,15 +47,13 @@ export const parseApproval = (approval: any): ApprovalAction => {
       approval.j6Status == "Declined" ||
       approval.pboStatus == "Declined"
     ) {
-      action = new ApprovalAction(ApprovalActions["reject"]);
+      type = "reject";
     } else {
-      action = new ApprovalAction(ApprovalActions["approve"]);
+      type = "approve";
     }
-    action.date = new Date();
-    action.formInputs["user"] = {};
 
     try {
-      action.formInputs["comments"] =
+      formInputs["comments"] =
         approval.directorateComment ||
         approval.boComment ||
         approval.j6Comment ||
@@ -50,7 +64,7 @@ export const parseApproval = (approval: any): ApprovalAction => {
         approval.supplyComment ||
         approval.j4Comment;
 
-      action.formInputs["userString"] =
+      formInputs["userString"] =
         approval.directorateSignature ||
         approval.boSignature ||
         approval.j6Signature ||
@@ -62,32 +76,32 @@ export const parseApproval = (approval: any): ApprovalAction => {
         approval.j4Signature;
 
       //SIGNED BY: i:0#.w|sof\kale.sawyer ON: Thu Oct 10 2019 09:19:20 GMT+0200 (W. Europe Daylight Time)
-      let decoded = decodeURIComponent(action.formInputs["userString"]);
+      let decoded = decodeURIComponent(formInputs["userString"]);
       let result = decoded.split(" ON: ");
-      let user = result[0].substring(11);
-      action.formInputs["userString"] = user;
-      let date = result[1];
-      action.formInputs["dateString"] = date;
-      action.date = new Date(date);
+      let userStr = result[0].substring(11);
+      formInputs["userString"] = userStr;
+      let dateStr = result[1];
+      formInputs["dateString"] = date;
+      date = new Date(dateStr);
     } catch (e) {
       console.error(`parseApproval`, e);
     }
   }
-  return action;
+  return { ...ApprovalActions[type], date: date, formInputs: formInputs };
 };
 
-//try to get the legacy data into a usable format
+//try to get the legacy data into a usable format. match each legacy approval to its friendly status,
+//parse the date/signature, and stuff that info into a RequestApproval
 export const convertApprovalsToHistory = (approvals: IRequestApprovals) => {
   let history: { [key: string]: IApprovalAction | undefined } = {};
-
-  for (var key in statuses) {
-    if (statuses.hasOwnProperty(key)) {
-      if (new Set(["Draft", "Submitted", "Closed"]).has(key)) continue;
-      const status = key;
+  for (var key in approvals) {
+    if (approvals.hasOwnProperty(key)) {
+      const status = getStatusForApprovalName(key);
       const approval = getApprovalForStatus(status, approvals);
-      let action = approval ? parseApproval(approval) : undefined;
+      let action: any = approval ? parseApproval(approval) : undefined;
       history[status] = action;
     }
   }
+
   return history;
 };
