4517f92619f4d221fe0ae0e750245b3d9359b11e
Ryan McLean <mcleanra@gmail.com>
2020-02-03 17:48:10 +0100

    refactor

--- src/services/models/Request.ts
+++ src/services/models/Request.ts
@@ -1,57 +1,13 @@
 import { autoserializeAs, autoserialize } from "cerialize";
 import { RequestField } from "./RequestField";
 import { LineItem } from "./LineItem";
-import { BudgetOfficerApproval } from "./BudgetOfficerApproval";
-import { BillingOfficialApproval } from "./BillingOfficialApproval";
-import { J6Approval } from "./J6Approval";
-import { PboApproval } from "./PboApproval";
-import { DirectorateApproval } from "./DirectorateApproval";
-import { J8Approval } from "./J8Approval";
-import { CardholderValidation } from "./CardholderValidation";
-import { RequestorValidation } from "./RequestorValidation";
-import { SupplyValidation } from "./SupplyValidation";
-import { FinalValidation } from "./FinalValidation";
 import { SharepointUser } from "./SharepointUser";
 import { ApprovalAction } from "./ApprovalAction";
 import { compareDesc, parseISO } from "date-fns";
-import * as Yup from "yup";
 import { ApprovalActions } from "../../constants/ApprovalActions";
 import { getStatusesByFriendlyName } from "../../constants/StepStatus";
-
-export interface IRequestApprovals {
-  [key: string]: any;
-}
-export class RequestApprovals implements IRequestApprovals {
-  @autoserializeAs(BudgetOfficerApproval)
-  budgetOfficerApproval?: BudgetOfficerApproval;
-
-  @autoserializeAs(BillingOfficialApproval)
-  billingOfficialApproval?: BillingOfficialApproval;
-
-  @autoserializeAs(J6Approval)
-  j6Approval?: J6Approval;
-
-  @autoserializeAs(PboApproval)
-  pboApproval?: PboApproval;
-
-  @autoserializeAs(DirectorateApproval)
-  directorateApproval?: DirectorateApproval;
-
-  @autoserializeAs(J8Approval)
-  j8Approval?: J8Approval;
-
-  @autoserializeAs(CardholderValidation)
-  cardholderValidation?: CardholderValidation;
-
-  @autoserializeAs(RequestorValidation)
-  requestorValidation?: RequestorValidation;
-
-  @autoserializeAs(SupplyValidation)
-  supplyValidation?: SupplyValidation;
-
-  @autoserializeAs(FinalValidation)
-  finalValidation?: FinalValidation;
-}
+import { RequestApprovals } from "./RequestApprovals";
+import * as Yup from "yup";
 
 export interface IRequest {
   id?: number;
