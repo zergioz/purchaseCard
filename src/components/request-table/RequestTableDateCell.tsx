cca70ecc935e28eabbd50346d1c02dbb5002d7d7
Ryan McLean <mcleanra@gmail.com>
2020-02-06 16:30:42 +0100

    added some error handling

--- src/components/request-table/RequestTableDateCell.tsx
+++ src/components/request-table/RequestTableDateCell.tsx
@@ -1,22 +1,36 @@
 import React from "react";
 import { format, formatDistance, parseISO } from "date-fns";
+import { ErrorBoundary } from "../error-boundary/ErrorBoundary";
 
+const formatStr = "dd MMM yyyy";
 interface IProps {
   dateISOString: string;
 }
-export const RequestTableDateCell = (props: IProps) => {
-  const formatStr = "dd-MMM-yyyy";
 
-  let date = parseISO(props.dateISOString);
-  const formattedDate = format(date, formatStr);
-  const distance = formatDistance(date, new Date(), {
-    addSuffix: true
-  });
+const RequestTableDateCellUnsafe = (props: IProps) => {
+  if (!props.dateISOString) return null;
+  let formattedDate = "";
+  let distance = "";
+  try {
+    let date = parseISO(props.dateISOString);
+    formattedDate = format(date, formatStr);
+    distance = formatDistance(date, new Date(), {
+      addSuffix: true
+    });
+  } catch (e) {
+    console.error(`RequestTableDateCellUnsafe(${props.dateISOString}): `, e);
+  }
   return (
     <div className="nowrap" style={{ whiteSpace: "pre" }}>
-      {formattedDate.toUpperCase()}
+      {formattedDate}
       <br />
       <small className="text-secondary">{distance}</small>
     </div>
   );
 };
+
+export const RequestTableDateCell = (props: IProps) => (
+  <ErrorBoundary>
+    <RequestTableDateCellUnsafe dateISOString={props.dateISOString} />
+  </ErrorBoundary>
+);
